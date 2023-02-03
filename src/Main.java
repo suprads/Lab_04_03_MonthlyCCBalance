b93add30dc3aa77d09c83671ae687f50c7ad9ed6
Dil <suprads@mail.uc.edu>
2023-02-03 14:05:06 -0500

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        int balance = 5000;
+        double interestRate = 0.17;
+
+        double oneMonthRate = balance + balance * interestRate;
+        System.out.println("The interest due after one month is $" + oneMonthRate);
+
+        double twoMonthRate = oneMonthRate + oneMonthRate * interestRate;
+        System.out.println("The interest due after two months is $" + twoMonthRate);
     }
 }
